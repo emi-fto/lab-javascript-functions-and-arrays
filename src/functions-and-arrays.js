f68225caa0d4690979fcd48b7b7af8c426196516
Emiliano Foti <foti.emiliano@gmail.com>
2023-11-23 18:05:51 +0100

    Reached iteration 4 for the time being

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,46 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1,num2) {
+  if (num1 > num2){
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
-
-
+function findLongestWord(arr) {
+  let longestWord = "";
+  /*for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].length > longestWord.length) {
+      longestWord = arr[i];
+    }
+  }*/
+  arr.forEach(function(plArr){
+    if (plArr.length > longestWord.length){
+      longestWord = plArr;
+    }
+  });
+  if (arr.length === 0) {
+    longestWord = null;
+  }
+  return longestWord;
+}
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  let numberSum = 0;
+  /*for (let i=0; i< arr.length; i +=1){
+    numberSum += arr[i];
+  }*/
+  arr.forEach(function(plArr){
+    numberSum += plArr;
+  });
+  return numberSum;
+}
 
 
 
@@ -26,13 +53,34 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(arr) {
+  let numberSum = 0;
+    arr.forEach(function (plArr) {
+    numberSum += plArr;
+    return numberSum;
+  });
+  let avg = numberSum / arr.length;
+    if (arr.length === 0) {
+    avg = null;
+  }
+  return avg;
+}
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) {
+  let wordsSum = 0;
+  arr.forEach(function (plArr) {
+    wordsSum += plArr.length;
+    return wordsSum;
+  });
+  let avg = wordsSum / arr.length;
+  if (arr.length === 0) {
+    avg = null;
+  }
+  return avg;
+}
 
 // Bonus - Iteration #4.1
 function avg() {}
